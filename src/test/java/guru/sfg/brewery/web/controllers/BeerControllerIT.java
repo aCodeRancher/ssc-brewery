b65bf5adc82a5b790dec93927e8481609d6209b3
helen <hhma@alumni.cpp.edu>
2020-12-21 11:30:10 -0500

    add unauthenticated test case

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -15,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -53,7 +54,7 @@ public class BeerControllerIT {
                 .build();
     }
 
-    @WithMockUser("spring")
+    @WithMockUser
     @Test
     void findBeers() throws Exception{
         mockMvc.perform(get("/beers/find"))
@@ -62,4 +63,18 @@ public class BeerControllerIT {
                 .andExpect(model().attributeExists("beer"));
     }
 
+    @Test
+    void findBeersWithHttpBasic() throws Exception{
+        mockMvc.perform(get("/beers/find").with(httpBasic("spring","guru")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/findBeers"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+    @Test
+    void findBeersWithHttpBasic_notauthenticated() throws Exception{
+        mockMvc.perform(get("/beers/find").with(httpBasic("invalid","invalid")))
+                .andExpect(status().isUnauthorized());
+
+    }
 }
