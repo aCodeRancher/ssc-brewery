c1f94c28f5c0093ad2a2ebd7cdf556d9dac82509
helen <hhma@alumni.cpp.edu>
2020-12-22 22:54:25 -0500

    add bcrypt15 as an encoder.

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -50,7 +50,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .password("{sha256}1296cefceb47413d3fb91ac7586a4625c33937b4d3109f5a4dd96c79c46193a029db713b96006ded")
                 .roles("USER");
 
-        auth.inMemoryAuthentication().withUser("scott").password("{ldap}{SSHA}A10yuLOEGbSTbHl7csQHk7X0X3rwrqdmBomRsA==").roles("CUSTOMER");
+        auth.inMemoryAuthentication().withUser("scott")
+                .password("{bcrypt15}$2a$15$1Y7oTa/gopuIFqfM7E2RxuJz/.GVggn6Yz7sYgu.egF.odAwd1je2").roles("CUSTOMER");
     }
 
     //    @Override
