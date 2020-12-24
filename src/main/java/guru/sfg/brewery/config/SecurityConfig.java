6021dfbb669205b9185f23c0fbd0175c31704bdc
helen <hhma@alumni.cpp.edu>
2020-12-24 14:30:58 -0500

    add two filters

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package guru.sfg.brewery.config;
 
+import guru.sfg.brewery.security.AbstractAuthFilter;
 import guru.sfg.brewery.security.RestHeaderAuthFilter;
 import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
+import guru.sfg.brewery.security.URLParamAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -21,18 +23,24 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    public RestHeaderAuthFilter restHeaderAuthFilter(AuthenticationManager authenticationManager){
+    public AbstractAuthFilter restHeaderAuthFilter(AuthenticationManager authenticationManager){
         RestHeaderAuthFilter filter = new RestHeaderAuthFilter(new AntPathRequestMatcher("/api/**"));
         filter.setAuthenticationManager(authenticationManager);
         return filter;
     }
 
+    public AbstractAuthFilter urlParamAuthFilter(AuthenticationManager authenticationManager){
+        AbstractAuthFilter filter = new URLParamAuthFilter(new AntPathRequestMatcher("/api/**"));
+        filter.setAuthenticationManager(authenticationManager);
+        return filter;
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
                 http.addFilterBefore(restHeaderAuthFilter(authenticationManager()),
                         UsernamePasswordAuthenticationFilter.class)
                 .csrf().disable();
-
+                http.addFilterBefore(urlParamAuthFilter(authenticationManager()),
+                UsernamePasswordAuthenticationFilter.class);
                 http
                 .authorizeRequests(authorize -> {
                     authorize
