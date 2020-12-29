93a6ca256c24986110360d2ebe66f38442a4ae9f
helen <hhma@alumni.cpp.edu>
2020-12-28 22:30:00 -0500

    add edit beer and save beer tests

--- src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -1,8 +1,11 @@
 package guru.sfg.brewery.web.controllers.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.sfg.brewery.bootstrap.DefaultBreweryLoader;
 import guru.sfg.brewery.domain.Beer;
 import guru.sfg.brewery.repositories.BeerRepository;
 import guru.sfg.brewery.web.controllers.BaseIT;
+import guru.sfg.brewery.web.model.BeerDto;
 import guru.sfg.brewery.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -11,12 +14,12 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 
 import java.util.Random;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -27,6 +30,9 @@ public class BeerRestControllerIT extends BaseIT {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @DisplayName("Delete Tests")
     @Nested
@@ -117,9 +123,104 @@ public class BeerRestControllerIT extends BaseIT {
         @ParameterizedTest(name = "#{index} with [{arguments}]")
         @MethodSource("guru.sfg.brewery.web.controllers.BeerControllerIT#getStreamAllUsers")
         void findBeerByUpcAUTH(String user, String pwd) throws Exception {
-            mockMvc.perform(get("/api/v1/beerUpc/0631234200036")
+            String upc = DefaultBreweryLoader.BEER_1_UPC;
+            mockMvc.perform(get("/api/v1/beerUpc/"+ upc)
                     .with(httpBasic(user, pwd)))
                     .andExpect(status().isOk());
         }
     }
+
+    @DisplayName("Save Beer")
+    @Nested
+    class SaveBeer{
+
+        @ParameterizedTest(name = "#{index} with [{arguments}]")
+        @MethodSource("guru.sfg.brewery.web.controllers.BeerControllerIT#getStreamAdmin")
+        void saveBeerAUTH(String user, String pwd) throws Exception {
+            BeerDto beerDto = BeerDto.builder().build();
+            String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+            mockMvc.perform(post("/api/v1/beer/")
+                    .with(httpBasic(user, pwd))
+                    .content(beerDtoJson)
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isCreated());
+        }
+
+        @ParameterizedTest(name = "#{index} with [{arguments}]")
+        @MethodSource("guru.sfg.brewery.web.controllers.BeerControllerIT#getStreamNotAdmin")
+        void saveBeerNotAdmin(String user, String pwd) throws Exception {
+            BeerDto beerDto = BeerDto.builder().build();
+            String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+            mockMvc.perform(post("/api/v1/beer/")
+                    .with(httpBasic(user, pwd))
+                    .content(beerDtoJson)
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isForbidden());
+        }
+
+         @Test
+        void saveBeerNotAuth( ) throws Exception {
+            BeerDto beerDto = BeerDto.builder().build();
+            String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+            mockMvc.perform(post("/api/v1/beer/")
+                    .content(beerDtoJson)
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isUnauthorized());
+        }
+    }
+
+    @DisplayName("Edit Beer")
+    @Nested
+    class EditBeer{
+
+        public Beer beerToEdit() {
+            Random rand = new Random();
+
+            return beerRepository.saveAndFlush(Beer.builder()
+                    .beerName("Edit Me Beer")
+                    .beerStyle(BeerStyleEnum.IPA)
+                    .minOnHand(12)
+                    .quantityToBrew(200)
+                    .upc(String.valueOf(rand.nextInt(99999999)))
+                    .build());
+        }
+
+        @ParameterizedTest(name = "#{index} with [{arguments}]")
+        @MethodSource("guru.sfg.brewery.web.controllers.BeerControllerIT#getStreamAdmin")
+        void editBeerAUTH(String user, String pwd) throws Exception {
+            BeerDto beerDto = BeerDto.builder().build();
+            String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+            mockMvc.perform(put("/api/v1/beer/"+ beerToEdit().getId())
+                    .with(httpBasic(user, pwd))
+                    .content(beerDtoJson)
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isNoContent());
+        }
+
+        @ParameterizedTest(name = "#{index} with [{arguments}]")
+        @MethodSource("guru.sfg.brewery.web.controllers.BeerControllerIT#getStreamNotAdmin")
+        void editBeerNotAdmin(String user, String pwd) throws Exception {
+            BeerDto beerDto = BeerDto.builder().build();
+            String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+            mockMvc.perform(put("/api/v1/beer/"+beerToEdit().getId())
+                    .with(httpBasic(user, pwd))
+                    .content(beerDtoJson)
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isForbidden());
+        }
+
+        @Test
+        void editBeerNotAuth( ) throws Exception {
+            BeerDto beerDto = BeerDto.builder().build();
+            String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+            mockMvc.perform(post("/api/v1/beer/"+beerToEdit().getId())
+                    .content(beerDtoJson)
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isUnauthorized());
+        }
+    }
+
 }
