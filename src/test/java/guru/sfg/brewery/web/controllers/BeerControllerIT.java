f29ffd9802acf728448eaa9405bd78de7c0576f0
helen <hhma@alumni.cpp.edu>
2020-12-31 15:30:02 -0500

    add tests with csrf for post operations.

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -2,6 +2,7 @@ package guru.sfg.brewery.web.controllers;
 
 import guru.sfg.brewery.domain.Beer;
 import guru.sfg.brewery.repositories.BeerRepository;
+import guru.sfg.brewery.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -9,10 +10,14 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import java.util.UUID;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
@@ -103,6 +108,86 @@ public class BeerControllerIT extends BaseIT{
 
             mockMvc.perform(get("/beers/" + beer.getId()))
                     .andExpect(status().isUnauthorized());
+        }
+    }
+
+    @DisplayName("Add Beers")
+    @Nested
+    class AddBeer {
+
+        @Rollback
+        @Test
+        void processCreationForm() throws Exception{
+            mockMvc.perform(post("/beers/new").with(csrf())
+                    .param("beerName", "DC Beer")
+                    .param("beerStyle", BeerStyleEnum.LAGER.name())
+                    .param("upc", "12345678")
+                    .param("minOnHand", "2")
+                    .param("quantityToBrew", "2")
+                    .param("price", "10.00")
+                    .with(httpBasic("spring", "guru")))
+                    .andExpect(status().is3xxRedirection());
+        }
+
+        @Rollback
+        @ParameterizedTest(name = "#{index} with [{arguments}]")
+        @MethodSource("guru.sfg.brewery.web.controllers.BeerControllerIT#getStreamNotAdmin")
+        void processCreationFormNOTAUTH(String user, String pwd) throws Exception{
+            mockMvc.perform(post("/customers/new")
+                    .param("beerName", "DC Beer")
+                    .param("beerStyle", BeerStyleEnum.LAGER.name())
+                    .param("upc", "12345678")
+                    .param("minOnHand", "2")
+                    .param("quantityToBrew", "2")
+                    .param("price", "10.00")
+                    .with(httpBasic(user, pwd)))
+                    .andExpect(status().isForbidden());
+        }
+
+        @Test
+        void processCreationFormAnonymous() throws Exception{
+            mockMvc.perform(post("/customers/new").with(csrf())
+                    .param("beerName", "DC Beer")
+                    .param("beerStyle", BeerStyleEnum.LAGER.name())
+                    .param("upc", "12345678")
+                    .param("minOnHand", "2")
+                    .param("quantityToBrew", "2")
+                    .param("price", "10.00"))
+                    .andExpect(status().isUnauthorized());
+        }
+    }
+
+    @DisplayName("Edit Beers")
+    @Nested
+    class EditBeer {
+
+        @Rollback
+        @Test
+        void processUpdateForm() throws Exception{
+            UUID beerId = beerRepository.findAll().get(0).getId();
+            mockMvc.perform(post("/beers/" + beerId +"/edit").with(csrf())
+                    .param("beerName", "DC Beer")
+                    .with(httpBasic("spring", "guru")))
+                    .andExpect(status().is3xxRedirection());
+        }
+
+        @Rollback
+        @ParameterizedTest(name = "#{index} with [{arguments}]")
+        @MethodSource("guru.sfg.brewery.web.controllers.BeerControllerIT#getStreamNotAdmin")
+        void processUpdateFormNOTAUTH(String user, String pwd) throws Exception{
+            UUID beerId = beerRepository.findAll().get(0).getId();
+            mockMvc.perform(post("/beers/"+beerId+ "/edit").with(csrf())
+                    .param("beerName", "DC Beer")
+                     .with(httpBasic(user, pwd)))
+                    .andExpect(status().isForbidden());
+        }
+
+        @Test
+        void processUpdateFormAnoymous() throws Exception{
+            UUID beerId = beerRepository.findAll().get(0).getId();
+            mockMvc.perform(post("/beers/"+beerId+ "/edit").with(csrf())
+                    .param("beerName", "DC Beer"))
+                     .andExpect(status().isUnauthorized());
         }
     }
 }
